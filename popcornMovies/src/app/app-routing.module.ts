54209f62cdff16aaaa9d77e2b5a9341684697565
himanshu14263 <himanshu14263@gmail.com>
2019-09-09 00:19:35 +0530

    routing and home page added.

--- popcornMovies/src/app/app-routing.module.ts
+++ popcornMovies/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule,NgZorroAntdModule]
 })
 export class AppRoutingModule { }
